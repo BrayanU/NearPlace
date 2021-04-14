c2192ab4c5c20d1ded6fa95804be0b7a6a145c71
OmarMejia <omejiaa@uninorte.edu.co>
2021-04-14 11:00:35 -0500

    correct class Mapa

--- lib/appmovilapp/mapa.dart
+++ lib/appmovilapp/mapa.dart
@@ -1,32 +1,48 @@
-import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Mapa extends StatefulWidget {
+  final LatLng center;
+  final Set<Marker> markers;
+  final int zoom;
+
+  const Mapa({
+    this.center,
+    this.zoom,
+    this.markers,
+  });
+
   @override
   _Mapa createState() => _Mapa();
 }
 
 class _Mapa extends State<Mapa> {
   GoogleMapController mapController;
-  Set<Marker> _markers = HashSet<Marker>();
-  int _markerIdCounter = 1;
-  final LatLng _center = const LatLng(10.9878, -74.7955);
+  Set<Marker> _markers;
+  LatLng _center;
+  int _zoom;
+
+  void initState() {
+    _markers = widget.markers;
+    _center = widget.center;
+    _zoom = widget.zoom;
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
-  void _setMarkers(LatLng point, String titleMarker, String descriptionMarker) {
-    final String markerIdVal = 'marker_id_$_markerIdCounter';
-    _markerIdCounter++;
-    _markers.add(
+  Set<Marker> _addMarker(Set<Marker> _Markers, LatLng point, String titleMarker,
+      String descriptionMarker) {
+    final String markerIdVal = 'marker_id_$point';
+    _Markers.add(
       Marker(
           markerId: MarkerId(markerIdVal),
           position: point,
           infoWindow:
               InfoWindow(title: '$titleMarker', snippet: '$descriptionMarker')),
     );
+    return _Markers;
   }
 
   @override
@@ -35,7 +51,7 @@ class _Mapa extends State<Mapa> {
       onMapCreated: _onMapCreated,
       initialCameraPosition: CameraPosition(
         target: _center,
-        zoom: 15,
+        zoom: _zoom.toDouble(),
       ),
       markers: _markers,
       // polylines: _polyline,
